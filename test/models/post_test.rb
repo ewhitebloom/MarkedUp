15dc3124d1a0e67f3471dc3a59c1e92b39a58b13
ewhitebloom <ewhitebloom@gmail.com>
2014-03-25 17:28:39 -0400

    spaces

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,28 +1,29 @@
 require 'spec_helper'
 
 describe Post do
- it 'should belong to a user' do
-  expect(post).to respond_to(:user)
-end
-it 'requires a user' do
- post.user = nil
- expect(post).to_not be_valid
- expect(post.errors[:user]).to include("can't be blank")
-end
-it 'requires a body' do
-  post.body = nil
-  expect(post).to_not be_valid
-  expect(post.errors[:body]).to include("can't be blank")
-end
-it 'does not allow a body to be more than 280 characters' do
- post.body = 'a' * 501
- expect(post).to_not be_valid
- expect(post.errors[:body]).to include("is too long (maximum is 500 characters)")
-end
-it 'has many comments' do
- post.save!
- expect(post).to respond_to(:comments)
- comment = Comment.create(user: user, body: 'hello', post:post)
- expect(post.comments).to include(comment)
-end
+   it 'should belong to a user' do
+    expect(post).to respond_to(:user)
+  end
+  it 'requires a user' do
+   post.user = nil
+   expect(post).to_not be_valid
+   expect(post.errors[:user]).to include("can't be blank")
+  end
+  it 'requires a body' do
+    post.body = nil
+    expect(post).to_not be_valid
+    expect(post.errors[:body]).to include("can't be blank")
+  end
+  it 'does not allow a body to be more than 280 characters' do
+   post.body = 'a' * 501
+   expect(post).to_not be_valid
+   expect(post.errors[:body]).to include("is too long (maximum is 500 characters)")
+  end
+  it 'has many comments' do
+   post.save!
+   expect(post).to respond_to(:comments)
+   comment = Comment.create(user: user, body: 'hello', post:post)
+   expect(post.comments).to include(comment)
+  end
+
 end
