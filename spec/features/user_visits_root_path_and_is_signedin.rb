ec50eb37451f03b02a5dcbaac76270a93643b722
ewhitebloom <ewhitebloom@gmail.com>
2014-03-28 17:22:51 -0400

    validations and more

--- spec/features/user_visits_root_path_and_is_signedin.rb
+++ spec/features/user_visits_root_path_and_is_signedin.rb
@@ -27,13 +27,14 @@ feature "User visits the root path, with the map and other features" do
       visit root_path
       fill_in "Category", with: 'Transportation'
       fill_in "Body", with: "some post content."
+      fill_in 'Location', location: '123 Maple Lane, Boston, MA 02145'
       click_button "Submit"
     end
 
     scenario "User sees all posts" do
-      post1 = Post.create!(category: 'Transportation', body: 'Leaf')
-      post2 = Post.create!(category: 'News', body: 'Electric')
-      post3 = Post.create!(category: 'Neighborhood', body: 'Fire')
+      post1 = Post.create!(category: 'Transportation', body: 'Leaf', location: '123 Maple Lane, Boston, MA 02145')
+      post2 = Post.create!(category: 'News', body: 'Electric', location: '123 Oak Lane, Somerville, MA 02145')
+      post3 = Post.create!(category: 'Neighborhood', body: 'Fire', location: '123 Mulberry Lane, Brookline, MA 02145')
       posts = [post1, post2, post3]
 
       visit root_path
