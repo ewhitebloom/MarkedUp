1eab84012b8a508bbbbde5c97d25f945ff1b047c
ewhitebloom <ewhitebloom@gmail.com>
2014-03-28 14:28:15 -0400

    j

--- spec/features/user_visits_about_page.rb
+++ spec/features/user_visits_about_page.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-scenario "User visits the about page" do
+feature "User visits the about page" do
   # As a user I want to visit the about page to learn about the site
 
   it "sees a title" do
--- spec/features/user_visits_root_path_and_is_signedin.rb
+++ spec/features/user_visits_root_path_and_is_signedin.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
-scenario "User visits the root path, with the map and other features" do
+feature "User visits the root path, with the map and other features" do
   # As a user visits the root path, he will see the map, and all associated features.
+  post1 = Post.create!(category: 'News', body: 'This is some post content.', latitude: 51.49271, longitude: -0.08995)
 
   it "sees a title" do
-    post1 = Post.create!(category: 'News', body: 'This is some post content.', latitude: 51.49271, longitude: -0.08995 )
     visit root_path
     expect(page).to have_content "MarkdUp"
+  end
+
+  it "" do
   end
 end
