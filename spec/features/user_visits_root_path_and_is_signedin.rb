bb6b21f1c53a4515add370668f3d81c1db845e19
ewhitebloom <ewhitebloom@gmail.com>
2014-03-28 14:54:11 -0400

    pending tests added for root page

--- spec/features/user_visits_root_path_and_is_signedin.rb
+++ spec/features/user_visits_root_path_and_is_signedin.rb
@@ -4,9 +4,19 @@ feature "User visits the root path, with the map and other features" do
   # As a user visits the root path, he will see the map, and all associated features.
 
   it "sees a title" do
-    post1 = Post.create!(category: 'News', body: 'This is some post content.', latitude: 51.49271, longitude: -0.08995)
     visit root_path
     expect(page).to have_content "MarkdUp"
   end
 
+  it 'can write a post' do
+    visit root_path
+    fill_in "Category", with: 'Transportation'
+    fill_in "Body", with: "some post content."
+    click_button "Submit"
+  end
+
+  it 'can navigate to about page'
+  it 'can navigate to users page'
+  it 'can navigate to profile page'
+  it 'can navigate to signup page'
 end
