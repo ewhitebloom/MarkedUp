bd94d0370302c0ff64778697fba996263d6f39c3
ewhitebloom <ewhitebloom@gmail.com>
2014-03-28 14:33:07 -0400

    little

--- spec/features/user_visits_root_path_and_is_signedin.rb
+++ spec/features/user_visits_root_path_and_is_signedin.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 feature "User visits the root path, with the map and other features" do
   # As a user visits the root path, he will see the map, and all associated features.
-  post1 = Post.create!(category: 'News', body: 'This is some post content.', latitude: 51.49271, longitude: -0.08995)
 
   it "sees a title" do
+    post1 = Post.create!(category: 'News', body: 'This is some post content.', latitude: 51.49271, longitude: -0.08995)
     visit root_path
     expect(page).to have_content "MarkdUp"
   end
