87178bb3b28fb3ee6d5ee13d27212489c5dc2a15
ewhitebloom <ewhitebloom@gmail.com>
2014-03-30 19:37:56 -0400

    spaces

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -20,11 +20,13 @@ describe Comment do
     expect(comment).to_not be_valid
     expect(comment.errors[:user]).to include("can't be blank")
   end
+
   it 'should validate presence of post' do
     comment.post = nil
     expect(comment).to_not be_valid
     expect(comment.errors[:post]).to include("can't be blank")
   end
+
   it 'should validate presence of body' do
     comment.body = nil
     expect(comment).to_not be_valid
