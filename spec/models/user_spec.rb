d41aa72ceba4a98e007bfec8cc62634cd17013be
ewhitebloom <ewhitebloom@gmail.com>
2014-03-30 20:16:07 -0400

    spaces

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  let(:valid_attributes){{first_name: 'Bob', last_name: 'Bill', email: 'bob@bill.com', address: '123 Candy Lane, Somerville, MA 02145', password: 'hello', password_confirmation: 'hello'}}
-  let(:user){User.new(valid_attributes)}
+  let(:valid_attributes){{ first_name: 'Bob', last_name: 'Bill', email: 'bob@bill.com', address: '123 Candy Lane, Somerville, MA 02145', password: 'hello', password_confirmation: 'hello' }}
+  let(:user){ User.new(valid_attributes)}
 
   it 'should have a valid email' do
     expect(user).to be_valid
