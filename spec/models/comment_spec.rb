8eaacc9dff9189558e6464d7e4f5a5a5e2b01a21
ewhitebloom <ewhitebloom@gmail.com>
2014-04-01 16:19:11 -0400

    organized for validations vs. assocaitions

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,31 +3,30 @@ require 'spec_helper'
 describe Comment do
   let(:comment){ FactoryGirl.create(:comment) }
 
-  it 'should belong to a user' do
-    expect(comment).to respond_to(:user)
-    expect(comment.user).to eql(user)
+  describe 'validations' do
+    it { should belong_to :user }
+
+    it { should belong_to :post }
+
+    it 'should validate presence of user' do
+      comment.user = nil
+      expect(comment).to_not be_valid
+      expect(comment.errors[:user]).to include("can't be blank")
+    end
+
+    it 'should validate presence of post' do
+      comment.post = nil
+      expect(comment).to_not be_valid
+      expect(comment.errors[:post]).to include("can't be blank")
+    end
+
+    it 'should validate presence of body' do
+      comment.body = nil
+      expect(comment).to_not be_valid
+      expect(comment.errors[:body]).to include("can't be blank")
+    end
   end
 
-  it 'should belong to a post' do
-    expect(comment).to respond_to(:post)
-    expect(comment.post).to eql(post)
-  end
-
-  it 'should validate presence of user' do
-    comment.user = nil
-    expect(comment).to_not be_valid
-    expect(comment.errors[:user]).to include("can't be blank")
-  end
-
-  it 'should validate presence of post' do
-    comment.post = nil
-    expect(comment).to_not be_valid
-    expect(comment.errors[:post]).to include("can't be blank")
-  end
-
-  it 'should validate presence of body' do
-    comment.body = nil
-    expect(comment).to_not be_valid
-    expect(comment.errors[:body]).to include("can't be blank")
+  describe 'associations' do
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,47 +3,52 @@ require 'spec_helper'
 describe Post do
  let(:post){ FactoryGirl.create(:post) }
 
- it { should belong_to :user}
+ describe 'validations' do
+   it { should belong_to :user}
 
- it 'requires a user' do
-   post.user = nil
+   it 'requires a user' do
+     post.user = nil
+     expect(post).to_not be_valid
+     expect(post.errors[:user]).to include("can't be blank")
+   end
+
+   it 'requires a body' do
+    post.body = nil
+    expect(post).to_not be_valid
+    expect(post.errors[:body]).to include("can't be blank")
+  end
+
+  it 'requires a category' do
+    post.category = nil
+    expect(post).to_not be_valid
+    expect(post.errors[:body]).to include("can't be blank")
+  end
+
+  it 'is geocoded' do
+    post.latitude = nil
+    post.longitude = nil
+    expect(post).to_not be_valid
+    expect(post.errors[:body]).to include("can't be blank")
+    post.address = nil
+    expect(post).to_not be_valid
+    expect(post.errors[:body]).to include("can't be blank")
+  end
+
+  it 'does not allow a body to be more than 180 characters' do
+   post.body = 'a' * 181
    expect(post).to_not be_valid
-   expect(post.errors[:user]).to include("can't be blank")
+   expect(post.errors[:body]).to include("is too long (maximum is 180 characters)")
  end
-
- it 'requires a body' do
-  post.body = nil
-  expect(post).to_not be_valid
-  expect(post.errors[:body]).to include("can't be blank")
 end
 
-it 'requires a category' do
-  post.category = nil
-  expect(post).to_not be_valid
-  expect(post.errors[:body]).to include("can't be blank")
-end
+describe 'assocations' do
 
-it 'is geocoded' do
-  post.latitude = nil
-  post.longitude = nil
-  expect(post).to_not be_valid
-  expect(post.errors[:body]).to include("can't be blank")
-  post.address = nil
-  expect(post).to_not be_valid
-  expect(post.errors[:body]).to include("can't be blank")
-end
-
-it 'does not allow a body to be more than 180 characters' do
- post.body = 'a' * 181
- expect(post).to_not be_valid
- expect(post.errors[:body]).to include("is too long (maximum is 180 characters)")
-end
-
-it 'has many comments' do
- post.save!
- expect(post).to have_many(:comments)
- comment = Comment.create!(user: user, body: 'hello', post: post)
- expect(post.comments).to include(comment)
+  it 'has many comments' do
+   post.save!
+   expect(post).to have_many(:comments)
+   comment = Comment.create!(user: user, body: 'hello', post: post)
+   expect(post.comments).to include(comment)
+ end
 end
 
 end
