91628049244f534c9c4cbd45a203cc227ac7700c
ewhitebloom <ewhitebloom@gmail.com>
2014-04-02 17:25:34 -0400

    added validated presence of address for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@ describe User do
   it { should have_valid(:address).when('123 Somerville St. Somerville, MA 02145', '123 Massachussetts Street, Boston, MA 02145') }
   it { should_not have_valid(:address).when('123 somerville st.', '123 somerville, ma')}
 
+  it { should validate_presence_of :address }
+
 
   describe 'associations' do
     it { should have_many :posts }
