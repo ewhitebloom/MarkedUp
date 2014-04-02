4817202b31d62423d4f710d639255fcbf4a7a6be
ewhitebloom <ewhitebloom@gmail.com>
2014-04-02 18:54:13 -0400

    model associations and validations added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ActiveRecord::Base
   validates :post_id, presence: true
   validates :body, presence: true
 
+  belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :user_id, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 180 }
   validates :category, presence: true
 
   acts_as_voteable
