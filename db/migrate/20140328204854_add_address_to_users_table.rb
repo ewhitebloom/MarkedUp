c60df90b19dd11bad506b98f0eefbd5239267153
ewhitebloom <ewhitebloom@gmail.com>
2014-04-01 20:58:58 -0400

    migration changes

--- db/migrate/20140328204854_add_address_to_users_table.rb
+++
@@ -1,9 +0,0 @@
-class AddAddressToUsersTable < ActiveRecord::Migration
-  def up
-    add_column :users, :address, :string, null: false
-  end
-
-  def down
-    remove_column :users, :address
-  end
-end
