c3d1f47ec60e6b0d04f3f1a1536ccece6354befc
ewhitebloom <ewhitebloom@gmail.com>
2014-04-02 22:08:49 -0400

    schema updated with migration change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328210815) do
+ActiveRecord::Schema.define(version: 20140328205313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20140328210815) do
     t.string   "user_id",    null: false
     t.string   "category",   null: false
     t.string   "body",       null: false
-    t.string   "address",    null: false
+    t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
     t.datetime "created_at"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20140328210815) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "address",                default: "", null: false
+    t.string   "first",                  default: "", null: false
+    t.string   "last",                   default: "", null: false
+    t.string   "latitude",               default: ""
+    t.string   "longitude",              default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -48,9 +53,6 @@ ActiveRecord::Schema.define(version: 20140328210815) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "address",                             null: false
-    t.float    "latitude"
-    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
