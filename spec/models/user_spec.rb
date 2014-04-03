b201ce602e24aa7e18100cab518a5d558ac65ca7
ewhitebloom <ewhitebloom@gmail.com>
2014-04-02 22:11:00 -0400

    user validations reorganized

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,13 @@ require 'spec_helper'
 describe User do
   let(:user){ FactoryGirl.create(:user) }
 
-  it { should have_valid(:address).when('123 Somerville St. Somerville, MA 02145', '123 Massachussetts Street, Boston, MA 02145') }
-  it { should_not have_valid(:address).when('123 somerville st.', '123 somerville, ma')}
+  describe 'Validations' do
+    it { should have_valid(:address).when('123 Somerville St. Somerville, MA 02145', '123 Massachussetts Street, Boston, MA 02145') }
+    it { should_not have_valid(:address).when('123 somerville st.', '123 somerville, ma')}
 
-  it { should validate_presence_of :address }
+    it { should validate_presence_of :address }
 
-
+  end
   describe 'Associations' do
     it { should have_many :posts }
     it { should have_many :comments }
