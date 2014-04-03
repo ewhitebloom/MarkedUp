ac0818537776644340bbc96e8ac5ed8a652fb2eb
ewhitebloom <ewhitebloom@gmail.com>
2014-04-03 11:17:36 -0400

    about page spec refactored, other specs refactored

--- spec/features/user_visits_about_page.rb
+++ spec/features/user_visits_about_page.rb
@@ -1,10 +1,38 @@
 require 'spec_helper'
 
-feature "User visits the about page" do
-  # As a user I want to visit the about page to learn about the site
+feature "User visits the about page", %Q{
+   As a registered or unregistered user,
+   I want to visit see the about page,
+   so that I can learn about the app.
+} do
+
+  # Acceptance Criteria:
+  #  * I can be registered or unregistered to view the page.
+  #  * I can see the page from any other page in the app.
+  #  * I can the title.
+  #  * I can see all content from the page.
+
 
   it "sees a title" do
     visit '/about'
     expect(page).to have_content "MarkdUp"
+  end
+
+  it 'can navigate to the about page when not signed in' do
+    visit '/about'
+    expect(page).to have_content 'MarkdUp is a geo-social network.'
+  end
+
+  it 'can navigate to the about page when signed in' do
+    user = FactoryGirl.create(:user)
+
+    visit '/'
+    click_on 'Sign In'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+
   end
 end
