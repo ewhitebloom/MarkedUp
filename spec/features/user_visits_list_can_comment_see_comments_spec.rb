35eafa9cfca7e2a0cf872be531cd1bc3c3778701
ewhitebloom <ewhitebloom@gmail.com>
2014-04-03 11:23:49 -0400

    add a comment spec

--- spec/features/user_visits_list_can_comment_see_comments_spec.rb
+++ spec/features/user_visits_list_can_comment_see_comments_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 
 feature 'Authenticated user can write and see other comments', %Q{
-   As a registered user,
-   I want to comment on a post,
-   so that I can give feedback to that post.
-} do
+ As a registered user,
+ I want to comment on a post,
+ so that I can give feedback to that post.
+ } do
 
   # Acceptance Criteria:
   #  * I must be an authenticated user.
   #  * I can click on the comments button below any post, to see a form for a new comment and also see other users' comments.
 
-  it 'can post a comment for a given post'
-  it 'can see other comments for a post'
+  within_frame 'list_view' do
 
+    it 'can post a comment for a given post' do
+      visit '/'
+    end
+
+    it 'can see other comments for a post'
+  end
 end
 
