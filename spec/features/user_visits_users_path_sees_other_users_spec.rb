22f966468ca2469aa2dc7ebc50bb1023e48eb474
ewhitebloom <ewhitebloom@gmail.com>
2014-04-03 10:22:13 -0400

    more specs added

---
+++ spec/features/user_visits_users_path_sees_other_users_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature "User visits the list page" do
+  # As a user, I should be able to see other users with an address within a certain mile radius of my address, so I can see who is in my local area on MarkedUp.
+
+  it 'should be able to see other users'
+
+end
+
+
