0e575ac0ee9df18a439683e6efe58305794c124d
ewhitebloom <ewhitebloom@gmail.com>
2014-04-03 10:03:33 -0400

    more feature specs

---
+++ spec/features/user_visits_list_can_comment_see_comments_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'User can write and see other comments' do
+#As a user, I should also be able to comment on a post in the list view by clicking on a 'Comment' button. Clicking the button will also display other user comments, in addition to the comment form.
+
+  it 'can post a comment for a given post'
+  it 'can see other comments for a post'
+
+end
+
---
+++ spec/features/user_visits_list_votes_post_up_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature "User visits the list page" do
+  # As a user, I should be able to vote up a post in the list view, as in the the map view.
+
+  it 'can vote a post up'
+
+end
