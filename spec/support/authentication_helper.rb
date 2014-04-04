fc3ff8cd3cd85cf853ef0137924e2d85ba6e0df2
ewhitebloom <ewhitebloom@gmail.com>
2014-04-03 20:49:51 -0400

    sign in and out helper refactored

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -2,8 +2,8 @@ module Sign_In_Sign_Out
 
   def sign_in_as(user)
     visit new_user_session_path
-    fill_in 'Email', with: 'jsmith@gmail.com'
-    fill_in 'Password', with: 'abigpassword234'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_on 'Sign In'
   end
 
