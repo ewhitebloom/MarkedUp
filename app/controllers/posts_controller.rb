e8ddd6087d772ed1c3eaee3bc9d90fb44cdb7708
ewhitebloom <ewhitebloom@gmail.com>
2014-04-05 23:29:26 -0400

    pages controller added, about page made

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MarkedUp::Application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
   resources :posts
+  match '/about', to: 'pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
