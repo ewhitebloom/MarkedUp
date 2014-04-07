78b0aa618253e4ac2bb456c6139e0cae13cb976d
ewhitebloom <ewhitebloom@gmail.com>
2014-04-06 21:11:58 -0400

    map is working

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,9 +4,12 @@ function initialize() {
 
     $.getJSON('/address.json', {}, function(location){
 
-      var coordinates = [parseFloat(location[0]),parseFloat(location[1])];
+      var map = L.map('map').setView([parseFloat(location[0]),parseFloat(location[1])], 14);
 
-      var map = L.map('map').setView(coordinates, 11);
+      L.tileLayer('http://{s}.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/997/256/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+      }).addTo(map);
 
     });
 
