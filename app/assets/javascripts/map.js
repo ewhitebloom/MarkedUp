a191245181bd4cbd789369f953525991d29bbe48
ewhitebloom <ewhitebloom@gmail.com>
2014-04-11 09:59:24 -0400

    foundation navbar created

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -6,11 +6,11 @@ function initializeMap() {
 
     var coordinates = [location[0],location[1]];
 
-    map = L.map('map').setView(coordinates, 14);
+    map = L.map('map').setView(coordinates, 16);
 
     L.tileLayer('http://{s}.tile.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/997/256/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-      maxZoom: 18
+      maxZoom: 22
     }).addTo(map);
 
     var marker = L.marker(coordinates).addTo(map);
