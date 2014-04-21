ed17b6ba381f10878c5b6b927ba2cd92082ce4b3
ewhitebloom <ewhitebloom@gmail.com>
2014-04-21 18:39:05 -0400

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,10 @@
 $(function(){ $(document).foundation(); });
 
 $(function() {
+  $("#posts").live("click", function() {
+    $.getScript(this.href);
+    return false;
+  });
    $("#posts_search input").keyup(function() {
      $.get($("#posts_search").attr("action"), $("#posts_search").serialize(), null, "script");
      return false;
